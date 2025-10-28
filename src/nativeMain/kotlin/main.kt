c02635afcf2d0cbac4b57e6b3a2598198aab630d
mole828 <mrx800@163.com>
2025-10-28 16:29:42 +0800

    log: 日志用法

--- src/nativeMain/kotlin/main.kt
+++ src/nativeMain/kotlin/main.kt
@@ -2,21 +2,42 @@ import io.ktor.http.ContentType
 import io.ktor.server.application.ApplicationStarted
 import io.ktor.server.application.log
 import io.ktor.server.cio.CIO
+import io.ktor.server.engine.addShutdownHook
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
+import io.ktor.util.logging.KtorSimpleLogger
+import io.ktor.util.logging.Logger
 import kotlin.time.Clock
 import kotlin.time.ExperimentalTime
+
+fun someFunction() {
+    throw NotImplementedError()
+}
+
+class TestClass {
+    val log: Logger = KtorSimpleLogger("TestClass")
+}
 
 @OptIn(ExperimentalTime::class)
 fun main() {
     val beginProgram = Clock.System.now()
-    embeddedServer(CIO, port = 8080, host = "127.0.0.1") {
+    val server = embeddedServer(CIO, port = 8080, host = "127.0.0.1") {
         routing {
             get("/") {
+                TestClass().log.info("c!")
                 log.info("hello, world!")
                 call.respondText("Hello, world!", ContentType.Text.Html)
+            }
+
+            get("/test/logger") {
+                call.respondText("log::class: ${log::class}")
+            }
+
+            get("/test/error") {
+                someFunction()
+                call.respondText("ok", ContentType.Text.Html)
             }
         }
     }.apply {
@@ -25,5 +46,9 @@ fun main() {
             val costTime = readyForServe - beginProgram
             app.log.info("prepare cost time: $costTime")
         }
-    }.start(wait = true)
+    }
+    server.addShutdownHook {
+        server.stop()
+    }
+    server.start(wait = true)
 }
