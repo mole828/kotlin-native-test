d82c682579de7b575e025ede2a489f415bd0eb88
mole828 <mrx800@163.com>
2025-12-28 19:37:11 +0800

    test: add a breakpoint test

---
+++ src/commonTest/kotlin/BreakPointTest.kt
@@ -0,0 +1,10 @@
+import kotlin.test.Test
+
+class BreakPointTest {
+    @Test
+    fun default() {
+        println("Hello, breakpoint test!")
+        val variable = "test"
+        println("This is a test breakpoint")
+    }
+}
